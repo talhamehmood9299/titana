835bd1fadd226391e315ba0e058657ec9d97d341
AZZ <136358504+AzzMedicalAssociates@users.noreply.github.com>
2024-03-25 05:15:14 -0400

    Update Albana.py

--- Albana.py
+++ Albana.py
@@ -176,50 +176,7 @@ class histroy_of_illness:
         history = lines[-1].strip()
 
         return history
-    def combine_the_text(self, basic_data, history, template):
-
-        system_2 = f"""
-        You are a medical assistant and you job is to write a history of illness of the patient.
-        The text contains the patient demographics, History of  disease or disorder, Medications and Doctor dictation.
-        Please write a History of illness based on the text delimited by the triple backticks.lets think step by step.
-        First line contains the patient demographics and provided 'history line'. Don't add the medications in this line.
-        Second line contains the patient current complains with prescribe medication if available.
-        Write the follow up information of disease or disorder in a line separately with only related medications.
-        It is necessary to concluded with "**No other medical concerns in today's appointment**".
-        Don't add the headings.
-        Don't repeat the medical history.
-        Utilize double asterisks for name, type of visit and medication.
-        Make sure all the provided text is added in the output, 
-        and nothing is missed in the output.
-
-    """
-        prompt_2 = f"""
-                Please write a History of illness in based on the text delimited by the triple backticks,\
-                ```{basic_data}```
-                the the patient history is delimited by triple dashes,
-                ---{history}---
-                other text is delimited by triple brackets.
-                {{{template}}}
-                and concluded with "No other medical concerns in today's appointment".
-                """
-        few_shot_1 = """Write a history of illness of the patient based on the text that I will provide"""
-        result_1 = """\
-        Calvin Mcrae, a 71-year-old male, came in for a follow-up visit. He has a medical history of Hypertension (HTN), Hypothyroidism, and a history of cellulitis of the face.\n \
-        Patient is following up on hypertension and hypothyroidism. He denies symptoms such as headaches, dizziness, diaphoresis, nausea/vomiting, fatigue, weakness, palpitations, leg cramps, peripheral edema, vision changes, chest pain, and shortness of breath. Mr. Mcrae is compliant with his medications which include Vitamin D2 1,250 mcg (50,000 unit) capsule, amlodipine 5 mg tablet, levothyroxine 50 mcg tablet, and cefadroxil 500 mg capsule. He denies any untoward side effects from these medications. He requested a medication refill during this visit.\n \
-
-        He is following up on diabetes. She denies experiencing symptoms such as polydipsia, polyphagia, and polyuria. There are also no reports of peripheral edema, vision changes, diaphoresis, tingling or numbness of the limbs, lesions on the feet, palpitations, chest pain, and shortness of breath. She is compliant with her medications which include OneTouch Ultra Test strips, OneTouch UltraSoft Lancets, metformin 500 mg tablet.\n \
-        He reported that he had a sexual encounter and his wife developed a yeast infection, which caused him concern. He is an occasional smoker. He also reported having a pimple or infected cyst, which may have developed after a sexual encounter. An EKG was performed and the records were noted.\n \
-        He had an infection of the upper lip and experienced irregular heartbeats. His conditions of importance include hypertension and hypothyroidism.\n \
-        **No other medical concerns in today's appointment**.\n \
-        """
-
-        messages_2 = [{'role': 'system', 'content': system_2},
-                      {'role': 'user', 'content': f"{self.delimiter}{few_shot_1}{self.delimiter}"},
-                      {'role': 'assistant', 'content': result_1},
-                      {'role': 'user', 'content': f"{self.delimiter}{prompt_2}{self.delimiter}"}]
-
-        response = get_completion(messages_2)
-        return response
+    
     def final(self):
         basic_data = self.get_basic_information()
 
