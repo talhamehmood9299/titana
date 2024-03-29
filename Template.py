2749241c3295f8ee8a364924c0a524de0e3f4637
AZZ <136358504+AzzMedicalAssociates@users.noreply.github.com>
2024-03-29 08:55:30 -0400

    Update Template.py

--- Template.py
+++ Template.py
@@ -12,7 +12,6 @@ import os
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 def get_templates(post_data):
-    # loader = TextLoader('./hpi.txt')
     loader = TextLoader("/code/Template_utf8.txt")
 
     documents = loader.load()
