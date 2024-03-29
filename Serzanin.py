0953d980f78d3611adfcd5d7aa458d4a3da0b33a
AZZ <136358504+AzzMedicalAssociates@users.noreply.github.com>
2024-03-29 05:26:23 -0400

    Update Serzanin.py

--- Serzanin.py
+++ Serzanin.py
@@ -1,7 +1,6 @@
 import openai
-from extra_functions import extract_text, get_completion, get_dictation
 from labs_radiology import get_lab_results
-from extra_functions import extract_text, get_completion,get_dictation
+from extra_functions import extract_text, get_completion, get_dictation, get_cpt_code
 from hpi import get_templates
 
 def task(task_string, post_date):
@@ -27,51 +26,13 @@ def task(task_string, post_date):
 
 class cpt_code:
     def __init__(self, post_date, delimiter="####"):
-        self.post_data = post_date
+        self.post_date = post_date
         self.delimiter = delimiter
         result = self.final()  # Call the final() method and store the result
         self.result = result  # Store the result as an attribute
 
     def final(self):
-        prompt = """You are a medical assistant. Your job is to make the python dictionary based on the text that I will provide.
-         The sequence of the keys are if available:
-         "Systolic:"
-         "Diastolic:"
-         "BMI:"
-         If the mentioned keys are not available than return the key "nothing" with the value "nothing". 
-        """
-        user_1 = """You are a medical assistant. Your job is to make the python dictionary based on the text that I will provide.
-         The sequence of the keys are if available:
-         "Systolic:"
-         "Diastolic:"
-         "BMI:"
-         If the mentioned keys are not available than return the key "nothing" with the value "nothing". 
-         """
-        result_1 = """
-        {'Systolic':'120', 'Diastolic':'80', 'BMI':'36.5'}
-        """
-        result_2 = """
-        {'nothing':'nothing'}
-        """
-        user_text = f"""
-        You are a medical assistant. Your job is to make the python dictionary based on the text delimited by triple backticks.
-             '''{self.post_data}'''
-         The sequence of the keys are if available:
-         "Systolic:"
-         "Diastolic:"
-         "BMI:"
-         If the mentioned keys are not available than return the key "nothing" with the value "nothing". 
-        """
-        messages = [
-            {'role': 'system', 'content': prompt},
-            {'role': 'user', 'content': f"{self.delimiter}{user_1}{self.delimiter}"},
-            {'role': 'assistant', 'content': f"{result_1}"},
-            {'role': 'user', 'content': f"{self.delimiter}{user_1}{self.delimiter}"},
-            {'role': 'assistant', 'content': f"{result_2}"},
-            {'role': 'user', 'content': f"{self.delimiter}{user_text}"}
-        ]
-        result = get_completion(messages)
-
+        result = get_cpt_code(self.post_date)
         return result
 
 class histroy_of_illness:
@@ -168,6 +129,7 @@ class histroy_of_illness:
         response = get_completion(messages_1)
         print(response)
 
+        response = response.content
         # Split the text into lines
         lines = response.split('\n')
 
@@ -178,13 +140,15 @@ class histroy_of_illness:
     def combine_the_text(self, basic_data, history, template):
 
         system_2 = f"""
-        You are a medical assistant and you job is to write a history of illness of the patient.
+        You are a medical assistant and your job is to write a history of illness of the patient.
         The text contains the patient demographics, History of  disease or disorder, Medications and Doctor dictation.
         Please write a History of illness based on the text delimited by the triple backticks.lets think step by step.
         First line contains the patient demographics and provided 'history line'. Don't add the medications in this line.
-        Second line contains the patient current complains with prescribe medication if available.
+        Second line always contain this sentence ``I introduced myself as her Healthcare Provider and the Medical Assistant will be doing Medical Documentation accompanying me.``.
+        Third line contains the medications which patient is currently using if available.
+        Fourth line contains the patient current complains with prescribe medications if available.
         Write the follow up information of disease or disorder in a line separately with only related medications.
-        It is necessary to concluded with "**No other medical concerns in today's appointment**".
+        If any medications are mentioned then It is necessary to concluded with "**She reports taking the following CNS medications** and then write the medications in separate lines".
         Don't add the headings.
         Don't repeat the medical history.
         Utilize double asterisks for name, type of visit and medication.
@@ -199,78 +163,92 @@ class histroy_of_illness:
                 ---{history}---
                 other text is delimited by triple brackets.
                 {{{template}}}
-                and concluded with "No other medical concerns in today's appointment".
+                If any medications are mentioned then It is necessary to concluded with "**She reports taking the following CNS medications** and then write medications in separate lines".".
                 """
-        few_shot_1 = """Write a history of illness of the patient based on the text that I will provide"""
+        few_shot_1 = """Michael Saavedra , 44 M pt hx of Anxiety and ADHD, taking Adderall 20mg BID, focused and doing well, denied SI/HI, panic attacks, loss of appetite or sleeping issues."""
         result_1 = """\
-        Calvin Mcrae, a 71-year-old male, came in for a follow-up visit. He has a medical history of Hypertension (HTN), Hypothyroidism, and a history of cellulitis of the face.\n \
-        Patient is following up on hypertension and hypothyroidism. He denies symptoms such as headaches, dizziness, diaphoresis, nausea/vomiting, fatigue, weakness, palpitations, leg cramps, peripheral edema, vision changes, chest pain, and shortness of breath. Mr. Mcrae is compliant with his medications which include Vitamin D2 1,250 mcg (50,000 unit) capsule, amlodipine 5 mg tablet, levothyroxine 50 mcg tablet, and cefadroxil 500 mg capsule. He denies any untoward side effects from these medications. He requested a medication refill during this visit.\n \
-
-        He is following up on diabetes. She denies experiencing symptoms such as polydipsia, polyphagia, and polyuria. There are also no reports of peripheral edema, vision changes, diaphoresis, tingling or numbness of the limbs, lesions on the feet, palpitations, chest pain, and shortness of breath. She is compliant with her medications which include OneTouch Ultra Test strips, OneTouch UltraSoft Lancets, metformin 500 mg tablet.\n \
-        He reported that he had a sexual encounter and his wife developed a yeast infection, which caused him concern. He is an occasional smoker. He also reported having a pimple or infected cyst, which may have developed after a sexual encounter. An EKG was performed and the records were noted.\n \
-        He had an infection of the upper lip and experienced irregular heartbeats. His conditions of importance include hypertension and hypothyroidism.\n \
-        **No other medical concerns in today's appointment**.\n \
+        Michael Saavedra is a 44-year-old male patient who has a history of Anxiety and ADHD. I introduced myself as her Healthcare Provider and the Medical Assistant will be doing Medical Documentation accompanying me.\n \
+        She has been taking Adderall 20mg BID for ADHD management with enough control\n \
+        She stays focused and doing well regarding her daily activities.\n \
+        She denied SI/HI, panic attacks, loss of appetite, or sleeping issues.\n \
+        **She reports taking the following CNS medications**\n \
+        Adderall 20mg BID\n \
         """
+
+        few_shot_2 = """Write a history of illness of the patient based on the text that I will provide"""
+        result_2 = """\
+               John Tkacs is a 62-year-old male patient who has a history of Anxiety. I introduced myself as his Healthcare Provider and the Medical Assistant will be doing Medical Documentation accompanying me.\n \
+               He was prescribed Alprazolam 0.5mg TID and Zoloft 50mg HS for anxiety management which he used with slight control\n \
+               He ran out of Alprazolam before the due date as he has been taking it more than the prescribed dose. He reported that he felt better with Zoloft 25mg and wants to be on it back.\n \
+               He Denied SI/HI, panic attacks, or AVH. No report of difficulty sleeping or loss of appetite.\n \
+               **She reports taking the following CNS medications**\n \
+               Alprazolam 0.5mg TID\n \
+               Zoloft 50mg HS\n \
+               """
+
+
 
         messages_2 = [{'role': 'system', 'content': system_2},
                       {'role': 'user', 'content': f"{self.delimiter}{few_shot_1}{self.delimiter}"},
                       {'role': 'assistant', 'content': result_1},
+                      {'role': 'user', 'content': f"{self.delimiter}{few_shot_2}{self.delimiter}"},
+                      {'role': 'assistant', 'content': result_2},
                       {'role': 'user', 'content': f"{self.delimiter}{prompt_2}{self.delimiter}"}]
 
         response = get_completion(messages_2)
         return response
     def final(self):
-        if "Type of visit: Lab/Radiology Review" in self.post_data:
-            basic_data = self.get_basic_information()
+        # if "Type of visit: Lab/Radiology Review" in self.post_data:
+        #     basic_data = self.get_basic_information()
+        #
+        #     history = self.get_history()
+        #
+        #     template = get_templates(basic_data)
+        #
+        #     general = self.combine_the_text(basic_data, history, template)
+        #     lab = get_lab_results()
+        #     final_response = f"{general}. {lab}"
+        #
+        #     few_shot_2 = """Write a history of illness of the patient based on the text that I will provide"""
+        #     result_2 = """\
+        #     This is a 42-year-old male who presented today for a follow-up on opioid dependence, ADHD, depression, anxiety, and Lab review.\n \
+        #
+        #     The patient is following up on Anxiety and continues on (Alprazolam 1mg) which is helping him with his symptoms. He can function and perform his ADL. He states he takes his medications regularly. He denies fatigue, body aches, n/v/d, and constipation. He denies chest pain and shortness of breath. He denies hallucinations, panic attacks, suicidal ideations, and dangerous ideations. He has a good appetite and sleep. The patient is following up on attention deficit disorder and continues on Adderall 5mg in combination with Adderall 20mg. Per the patient, Adderall is helping him to focus and concentrate while reading and doing other tasks. Denies headache or dizziness. Requesting for a prescription renewal.\n \
+        #
+        #     He is also following up on Lab review results show that Glucose is 100, WBC count 2.9, Absolute 986,\n \
+        #     eGFR is 110, Creatinine is 0.88, WHITE BLOOD CELL COUNT 2.9 HEMOGLOBIN A1c 5.1\n \
+        #
+        #     **No other medical concerns in today's appointment**.\n \
+        #     """
+        #
+        #     system_1 = (f""" You are a medical assistant and you job is to write a history of illness of the patient.
+        #     The text contains the patient demographics, History of  disease or disorder, Medications and Doctor dictation.
+        #     Please write a History of illness based on the text delimited by the triple backticks.lets think step by step.
+        #     First line contains the patient demographics and provided 'history line'. Don't add the medications in this line.
+        #     Second line contains the patient current complains with prescribe medication if available.
+        #     Write the follow up information of disease or disorder in a line separately with only related medications.
+        #     It is necessary to concluded with "**No other medical concerns in today's appointment**".
+        #     Don't add the headings.
+        #     Don't repeat the lines.
+        #     Utilize double asterisks for name, type of visit and medication.
+        #     Make sure all the provided text is added in the output.
+        #     and nothing is missed in the output.
+        #                 """)
+        #     messages_4 = [{'role': 'system', 'content': system_1},
+        #                   {'role': 'user', 'content': f"{self.delimiter}{few_shot_2}{self.delimiter}"},
+        #                   {'role': 'assistant', 'content': result_2},
+        #                   {'role': 'user', 'content': f"{self.delimiter}{final_response}{self.delimiter}"}]
+        #     response = get_completion(messages_4)
+        #     return response
+        # else:
+        basic_data = self.get_basic_information()
 
-            history = self.get_history()
+        history = self.get_history()
 
-            template = get_templates(basic_data)
+        template = get_templates(str(basic_data))
 
-            general = self.combine_the_text(basic_data, history, template)
-            lab = get_lab_results()
-            final_response = f"{general}. {lab}"
-
-            few_shot_2 = """Write a history of illness of the patient based on the text that I will provide"""
-            result_2 = """\
-            This is a 42-year-old male who presented today for a follow-up on opioid dependence, ADHD, depression, anxiety, and Lab review.\n \
-
-            The patient is following up on Anxiety and continues on (Alprazolam 1mg) which is helping him with his symptoms. He can function and perform his ADL. He states he takes his medications regularly. He denies fatigue, body aches, n/v/d, and constipation. He denies chest pain and shortness of breath. He denies hallucinations, panic attacks, suicidal ideations, and dangerous ideations. He has a good appetite and sleep. The patient is following up on attention deficit disorder and continues on Adderall 5mg in combination with Adderall 20mg. Per the patient, Adderall is helping him to focus and concentrate while reading and doing other tasks. Denies headache or dizziness. Requesting for a prescription renewal.\n \
-
-            He is also following up on Lab review results show that Glucose is 100, WBC count 2.9, Absolute 986,\n \
-            eGFR is 110, Creatinine is 0.88, WHITE BLOOD CELL COUNT 2.9 HEMOGLOBIN A1c 5.1\n \
-
-            **No other medical concerns in today's appointment**.\n \
-            """
-
-            system_1 = (f""" You are a medical assistant and you job is to write a history of illness of the patient.
-            The text contains the patient demographics, History of  disease or disorder, Medications and Doctor dictation.
-            Please write a History of illness based on the text delimited by the triple backticks.lets think step by step.
-            First line contains the patient demographics and provided 'history line'. Don't add the medications in this line.
-            Second line contains the patient current complains with prescribe medication if available.
-            Write the follow up information of disease or disorder in a line separately with only related medications.
-            It is necessary to concluded with "**No other medical concerns in today's appointment**".
-            Don't add the headings.
-            Don't repeat the lines.
-            Utilize double asterisks for name, type of visit and medication.
-            Make sure all the provided text is added in the output.
-            and nothing is missed in the output.
-                        """)
-            messages_4 = [{'role': 'system', 'content': system_1},
-                          {'role': 'user', 'content': f"{self.delimiter}{few_shot_2}{self.delimiter}"},
-                          {'role': 'assistant', 'content': result_2},
-                          {'role': 'user', 'content': f"{self.delimiter}{final_response}{self.delimiter}"}]
-            response = get_completion(messages_4)
-            return response
-        else:
-            basic_data = self.get_basic_information()
-
-            history = self.get_history()
-
-            template = get_templates(basic_data)
-
-            general = self.combine_the_text(basic_data, history, template)
-            return general
+        general = self.combine_the_text(basic_data, history, template)
+        return general.content
 
 
 class plan_of_care:
