c811a36eca85644fbaa2691a2195d4f9b4746c3b
Amanullah.Awan <amaanullah.azz@gmail.com>
2024-04-29 09:51:24 -0400

    update the physical exam, review of system and plan of care

--- Ahmad.py
+++ Ahmad.py
@@ -35,6 +35,7 @@ class cpt_code:
         result = get_cpt_code(self.post_date)
         return result
 
+
 class histroy_of_illness:
     def __init__(self, post_date, delimiter="####"):
         self.post_date = post_date
@@ -137,8 +138,8 @@ class histroy_of_illness:
         history = lines[-1].strip()
 
         return history
-    def combine_the_text(self, basic_data, history, template):
 
+    def combine_the_text(self, basic_data, history, template):
         system_2 = f"""
         You are a medical assistant and you job is to write a history of illness of the patient.
         The text contains the patient demographics, History of  disease or disorder, Medications and Doctor dictation.
@@ -181,6 +182,7 @@ class histroy_of_illness:
 
         response = get_completion(messages_2)
         return response
+
     def final(self):
         # if "Type of visit: Lab/Radiology Review" in self.post_date:
         #     basic_data = self.get_basic_information()
@@ -235,3 +237,250 @@ class histroy_of_illness:
 
         general = self.combine_the_text(basic_data, history, template)
         return general
+
+
+class review_of_system:
+    def __init__(self, post_date, delimiter="####"):
+        self.post_date = post_date
+        self.delimiter = delimiter
+        result = self.final()
+        self.result = result
+
+    def get_symptoms(self):
+        user_text = f"""
+                ```{self.post_date}```
+                You are a medical assistant. Your job is to identify the symptoms from the provided text delimited by triple backticks\
+                Next, extract and compile symptoms from the symptoms_list\
+                that are possibly related to the identified symptoms. \
+                Don't suggest any symptoms if it is not mentioned in the symptoms_list. \
+                Also add the symptoms in the output list that are not in the symptoms_list. \
+                It is mandatory that output should be in Python list.
+                Return empty list if no symptoms are present in the provided text.
+                """
+        symptoms_list = """
+            Your job is to extract the one symptom from this symptoms_list based on the identified symptoms.
+            symptoms_list = [
+                "Rashes", "Earache", "Discharge (possibly ear or nose related)",
+                "Ringing or decreased hearing", "Sneezing", "Sore throat",
+                "Stuffy or runny nose", "Congestion", "Cough", "Shortness of breath",
+                "Wheezing", "Mass or lump (possibly in the breast)",
+                "Abdominal pain", "Change in bowel habits", "Constipation",
+                "Diarrhea", "GERD (Gastro-esophageal Reflux Disease)", "Nausea",
+                "Vomiting", "Dysuria (painful urination)",
+                "Discharges (possibly genitourinary related)", "Cyanosis", "Edema",
+                "Dizziness", "Headache", "Neuropathic pain", "Numbness", "Paralysis",
+                "Seizures", "Tremors", "Back pain", "Joint pain", "Joint swelling",
+                "Muscle cramps", "Muscle weakness", "Anxiety", "Depression",
+                "Insomnia", "Opioid dependence", "Chest pain", "Palpitations",
+                "Blurred vision", "Vision loss", "Eye discharge", "Eye itching",
+                "Eye pain", "Abnormal bruising", "Bleeding", "Lymphadenopathy (enlarged lymph nodes)",
+                "Excessive thirst", "Excessive hunger", "Heat intolerance", "Cold intolerance", "Hypertension",
+                "ADHD", "Diabetes Mellitus", "Post-Traumatic Stress Disorder",
+                "Asthma", "Urinary Tract Infection", "Migraine (Neurologic)",
+                "Convulsions (Neurologic)", "Joint Pain", "Arthritis", "Osteoarthritis",
+                "Pain in Hand", "Pain in Foot", "Knee Pain", "Opioid Dependence",
+                "Depressive Disorder", "Stress Disorder", "Hyperlipidemia",
+                "Hyperthyroidism", "Hypothyroidism"
+            ]
+        """
+        few_shot_user_1 = """
+         You are a medical assistant. Your job is to identify the symptoms from the provided text delimited by triple backticks\
+                        Next, extract and compile symptoms from the symptoms_list\
+                        that is possibly related to the identified symptoms.
+        """
+
+        few_shot_assistant_1 = """
+        ["Shortness of breath", "Cough"]
+        """
+        messages = [{'role': 'system', 'content': symptoms_list},
+                    {'role': 'user', 'content': f"{self.delimiter}{few_shot_user_1}{self.delimiter}"},
+                    {'role': 'assistant', 'content': few_shot_assistant_1},
+                    {'role': 'user', 'content': f"{self.delimiter}{user_text}{self.delimiter}"}]
+
+        response = get_completion(messages)
+
+        return response
+
+    def final(self):
+        symptoms = self.get_symptoms()
+        patient_data = """
+            "**Skin:** Patient denies rashes",
+            "**Ear/nose/throat:** Patient denies, Earache or discharge, Ringing or decreased hearing, Sneezing, Sore throat, Stuffy or runny nose",
+            "**Lungs:** Patient denies, Congestion, cough, Shortness of breath, Wheezing",
+            "**Breast:** Patient denies, Mass or Lump",
+            "**Abdomen:** Patient denies, Abdominal pain, change in bowel habits, Constipation, Diarrhea, GERD, Nausea, Vomiting",
+            "**GenitoUrinary:** Patient denies, Dysuria, Discharges",
+            "**Extremities:** Patient denies, Cyanosis, Edema",
+            "**Neurologic:** Patient denies, Dizziness, Headache, Neuropathic pain, Numbness, paralysis, Seizures, Tremors",
+            "**Musculoskeletal:** Patient denies, Back pain, Joint pain, Joint swelling, Muscle cramps, Muscle weakness, Breakthrough pain",
+            "**Psychiatric:** Patient denies, Anxiety, Depression, Insomnia, Opioid dependence",
+            "**Cardiovascular:** Patient denies, Chest pain, Palpitation",
+            "**Eyes:** Patient denies, Blurred or vision loss, Discharge itching or eye pain",
+            "**Heme/lymphatic:** Patient denies, Abnormal bruising, Bleeding, Lymphadenopathy",
+            "**Endocrine:** Patient denies, Excessive thirst or hunger, Heat or cold intolerance, Skin or hair changes, Weight gain or loss",
+            "**Dentistry:** Patient denies, Toothache"
+        """
+        system = """
+        you are a medical assistant. Your job is to upgrade the template lines based on the symptoms lines based on the symptoms
+        after adding the line "patient complains of "Symptom" at the end of the that line not in the middle.
+        Return the template with no changes if no symptoms are given in the provided text.
+        Don't add extra text in the output.
+        """
+        user_text = f"""
+         You are a medical assistant. Your job is to rewrite the template lines
+         {patient_data} after adding the line "patient complains of "symptom"
+         based on the symptoms list delimited by triple backticks.
+         '''{symptoms}'''
+        Use double asterisks for the heading and the added line.
+        Make sure all the provided text is added in the output.
+        """
+        few_shot_user_1 = """
+        You are a medical assistant. Your job is to upgrade the template lines based on the symptoms lines based on the symptoms
+        after adding the line "patient complains of "Symptom"
+        """
+        few_shot_assistant_1 = """
+            **Skin:** Patient denies rashes.
+            **Ear/nose/throat:** Patient denies, Earache or discharge, Ringing or decreased hearing, Sneezing, Sore throat, Stuffy or runny nose.
+            **Lungs:** Patient denies, Congestion, cough, Shortness of breath, Wheezing.
+            **Breast:** Patient denies, Mass or Lump".
+            **Abdomen:** Patient denies, Abdominal pain, change in bowel habits, Constipation, Diarrhea, GERD, Nausea, Vomiting.
+            **GenitoUrinary:** Patient denies, Dysuria, Discharges".
+            **Extremities:** Patient denies, Cyanosis, Edema".
+            **Neurologic:** Patient denies, Dizziness, Headache, Neuropathic pain, Numbness, paralysis, Seizures, Tremors.
+            **Musculoskeletal:** Patient denies, Joint pain, Joint swelling, Muscle cramps, Muscle weakness, Breakthrough pain. **Patient complains of back pain**
+            **Psychiatric:** Patient denies, Anxiety, Depression, Insomnia, Opioid dependence.
+            **Cardiovascular:** Patient denies, Chest pain, Palpitation.
+            **Eyes:** Patient denies, Blurred or vision loss, Discharge itching or eye pain.
+            **Heme/lymphatic:** Patient denies, Abnormal bruising, Bleeding, Lymphadenopathy.
+            **Endocrine:** Patient denies, Excessive thirst or hunger, Heat or cold intolerance, Skin or hair changes, Weight gain or loss.
+            **Dentistry:** Patient denies, Toothache.
+        """
+        few_shot_user_2 = """
+        You are a medical assistant. Your job is to upgrade the template lines based on the symptoms lines based on the symptoms
+        after adding the line "patient complains of "Symptom"
+        """
+        few_shot_assistant_2 = """
+            **Skin:** Patient denies rashes.
+            **Ear/nose/throat:** Patient denies, Earache or discharge, Ringing or decreased hearing, Sneezing, Sore throat, Stuffy or runny nose.
+            **Lungs:** Patient denies, Congestion, cough, Shortness of breath, Wheezing.
+            **Breast:** Patient denies, Mass or Lump".
+            **Abdomen:** Patient denies, Abdominal pain, change in bowel habits, Constipation, Diarrhea, GERD, Nausea, Vomiting.
+            **GenitoUrinary:** Patient denies, Dysuria, Discharges".
+            **Extremities:** Patient denies, Cyanosis, Edema".
+            **Neurologic:** Patient denies, Dizziness, Headache, Neuropathic pain, Numbness, paralysis, Seizures, Tremors.
+            "**Musculoskeletal:** Patient denies, Back pain, Joint pain, Joint swelling, Muscle cramps, Muscle weakness, Breakthrough pain".
+            **Psychiatric:** Patient denies, Anxiety, Depression, Insomnia, Opioid dependence.
+            **Cardiovascular:** Patient denies, Chest pain, Palpitation.
+            **Eyes:** Patient denies, Blurred or vision loss, Discharge itching or eye pain.
+            **Heme/lymphatic:** Patient denies, Abnormal bruising, Bleeding, Lymphadenopathy.
+            **Endocrine:** Patient denies, Excessive thirst or hunger, Heat or cold intolerance, Skin or hair changes, Weight gain or loss.
+            **Dentistry:** Patient denies, Toothache.
+        """
+        messages = [{'role': 'system', 'content': system},
+                    {'role': 'user', 'content': f"{self.delimiter}{few_shot_user_1}{self.delimiter}"},
+                    {'role': 'assistant', 'content': few_shot_assistant_1},
+                    {'role': 'user', 'content': f"{self.delimiter}{few_shot_user_2}{self.delimiter}"},
+                    {'role': 'assistant', 'content': few_shot_assistant_2},
+                    {'role': 'user', 'content': f"{self.delimiter}{user_text}{self.delimiter}"}]
+
+        response = get_completion(messages)
+
+        return response
+
+
+class physical_exam:
+    def __init__(self, post_date, delimiter="####"):
+        self.post_data = post_date
+        self.delimiter = delimiter
+        result = self.final()
+        self.result = result
+
+    def final(self):
+        response_1 = ""
+        if "Type of visit: Follow Up" in self.post_data:
+            response_1 = "Patient is AAO x 3. Not in acute distress. Breathing is non-labored, Normal respiratory effort. The affect is normal and appropriate."
+        else:
+            response_1 = "Currently I can only provide physical exam for Type of visit: Follow Up"
+        return response_1
+
+
+class plan_of_care:
+    def __init__(self, post_date, delimiter="####"):
+        self.post_data = post_date
+        self.delimiter = delimiter
+        result = self.final()
+        self.result = result
+
+    def final(self):
+        prompt = """
+            you are a medical assistant. You job is to write a plan of care by following the mentioned rules. let's think step by step.
+                1) First extract the only one associate disease or disorder for each medication if mentioned in the provided text.
+                2) And than write all the medications in the first line separated with comma and write associated disease and disorder with lines plan of care and medication mentioned in the provided text.
+                3) Write the one name of the disease or disorder as the heading and than write lines of plan of care in the form of paragraph.
+                4) Write the short form of disease or disorders
+                5) Include only medication names. Don't add dosage, and SIG (instructions for use).
+                6) Don't add ICD-10 codes.
+                7) Don't add Start Date, Prescribe Date, End Date and Qty of the medication.
+                8) If the disease and disorder is not related with the medication, than don't add the disease or disorder in the \
+                output.
+                9) Add a concise plan of care with 4 to 5 lines for disease or disorder that i will provide.
+                10) Utilize double asterisks for all headings.
+                11) Utilize double asterisks for all "medications".
+                12) Don't suggest any disease, disorder or symptoms for any medication. 
+                13) Don't add heading of "Plan of care, Disease or disorder and "Medication".
+                14) Don't add the disease or disorder in the output if no medication is mentioned in the provided text.
+                15) If no medication is mentioned in the provided text than return these lines and also add at the end of every output if the medication is available.
+                "**Compliance** with medications has been stressed.
+                Continue other medications.
+                Side effects, risks, and complications were clearly explained and the patient verbalized understanding and was given the opportunity to ask questions.
+                Relaxation techniques were discussed, stress avoidance was reviewed, and medication and yoga were encouraged."
+                16) It is Mandatory to conclude the plan of care with this line "Follow-up as scheduled"
+            """
+
+        user_text = f"""
+               Write a concise plan of care with 4 to 5 lines for disease or disorder if the related medication and the doctor dication is linked with it.
+               It is mandatory to write only one heading of disease or disorder for one mentioned medication.
+               Don't add the disease or disorder in the output if no medication is mentioned in the provided text.
+               The disease or disorders, medications and doctor dictations are delimited by triple backticks.
+               '''{self.post_data}'''
+
+                """
+        few_shot_user_1 = """
+            Write a plan of care. Don't add the output in the future responses. 
+            write only one heading of disease or disorder for one mentioned medication.
+    
+            """
+        few_shot_user_2 = """
+            Write a plan of care. Don't add the output in the future responses.
+            Write only one heading of disease or disorder for one mentioned medication.
+    
+            """
+        few_shot_assistant_1 = """
+            Continue with metformin and levothyroxine.
+            **Hyperlipidemia**: Dietary instruction about hyperlipidemia given to the patient. Eat a diet low in saturated and trans fats. Include lots of fruits, vegetables, beans, nuts, whole grains, and fish regularly into your diet. Stop eating red meat and processed meats like bacon, sausage, and cold cuts. Drink skim or low-fat milk.
+            **GERD**: Recommended to eat slowly, chew well every bite for at least 20-25 times before swallowing. Do not talk, read or watch TV while eating. Eat small portions, frequently, at least every 3-h. No large meals. Avoid Tobacco, alcohol, greasy, acidic, or spicy food, coffee, and carbonated beverages. Sip plain water between bites. Avoid eating within 2 hours before bedtime. Walk after the meal.
+            Compliance with medications has been stressed. Continue other medications. Side effects, risks, and complications were clearly explained and the patient verbalized understanding and was given the opportunity to ask questions. Relaxation techniques were discussed, stress avoidance was reviewed, medication and yoga were encouraged.
+    
+            **Follow-up as scheduled**.
+    
+            """
+        few_shot_assistant_2 = """
+            "**Compliance** with medications has been stressed.
+            Continue other medications.
+            Side effects, risks, and complications were clearly explained and the patient verbalized understanding and was given the opportunity to ask questions.
+            Relaxation techniques were discussed, stress avoidance was reviewed, and medication and yoga were encouraged."
+    
+            **Follow-up as scheduled**.
+    
+            """
+
+        messages = [{'role': 'system', 'content': prompt},
+                    {'role': 'user', 'content': f"{self.delimiter}{few_shot_user_1}{self.delimiter}"},
+                    {'role': 'assistant', 'content': few_shot_assistant_1},
+                    {'role': 'user', 'content': f"{self.delimiter}{few_shot_user_2}{self.delimiter}"},
+                    {'role': 'assistant', 'content': few_shot_assistant_2},
+                    {'role': 'user', 'content': f"{self.delimiter}{user_text}{self.delimiter}"}]
+
+        response = get_completion(messages)
+
+        return response
