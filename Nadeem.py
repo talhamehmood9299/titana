03a973942aee521fd63faa2929eec0bad83cc0d6
Amanullah.Awan <amaanullah.azz@gmail.com>
2024-05-23 09:19:33 -0400

    changes in shahzinah nadeem

--- Nadeem.py
+++ Nadeem.py
@@ -46,39 +46,40 @@ class histroy_of_illness:
 
     def final(self):
         system_2 = f"""
-                            You are a medical assistant and you job is to write a history of illness of the patient.
-                            The text contains the patient demographics and Doctor dictation.
-                            Please write a History of illness based on the text delimited by the triple backticks.lets think step by step.
-                            First line contains the patient demographics. Don't add the medications in this line.
-                            Second line contains the patient current complains.
-                            It is necessary to concluded with "**No other medical concerns in today's appointment**".
-                            Don't add the headings.
-                            Don't repeat the lines.
-                            Use double astrikes for all the patient name and medication.
-                            Write lines separately.
+        You are a medical assistant and you job is to write a history of illness of the patient.
+        The text contains the patient demographics and Doctor dictation.
+        Please write a History of illness based on the text delimited by the triple backticks.lets think step by step.
+        First line contains the patient demographics. Don't add the medications in this line.
+        Second line contains the patient current complains.
+        It is necessary to concluded with "**No other medical concerns in today's appointment**".
+        Don't add the headings.
+        Ensure that all information is included in the response.
+        Don't repeat the lines.
+        Use double astrikes for all the patient name and medication.
+        Write lines separately.
                         """
         prompt_2 = f"""
-                                    Please write a History of illness in based on the text delimited by the triple backticks,\
-                                    ```{self.post_data}```
-                                    and concluded with "No other medical concerns in today's appointment".
-                                    """
+        Please write a History of illness in based on the text delimited by the triple backticks,\
+        ```{self.post_data}```
+        and concluded with "No other medical concerns in today's appointment".
+        """
         few_shot_1 = """Smith, Dolores, 76 F. pt flwng pain mngnm, nt,, severe lbp, pain mngmnt not much help, anxious restless, \
          not sleepmng, flwng rglrly pain mmngmnt. dfclty sleepng at ni8. 140/85 after repeat bp n taking clonodine."""
         result_1 = """\
-                            Dolores Smith, a 76-year-old female, presented for an office visit. \n \
-                            She has been following pain management for severe low back pain, but it hasn't been much help. \n \
-                            She is anxious, restless, and has difficulty sleeping at night. \n \
-                            Her blood pressure after repeat and taking clonidine was 140/85. \n \
-                            **No other medical concerns in today's appointment.**\n \
-                            """
+        Dolores Smith, a 76-year-old female, presented for an office visit. \n \
+        She has been following pain management for severe low back pain, but it hasn't been much help. \n \
+        She is anxious, restless, and has difficulty sleeping at night. \n \
+        Her blood pressure after repeat and taking clonidine was 140/85. \n \
+        **No other medical concerns in today's appointment.**\n \
+        """
         few_shot_2 = """Write a history of illness of the patient based on the text that I will provide"""
         result_2 = """\
-                            Godoy Sergio, a 39-year-old male, presented for a follow-up visit. \n \
-                            The patient has a history of HTN, and he is taking his BP readings as an outpatient, his recent BP reading was 120/76, his heart rate was 68, and his current weight is 203 pounds. \n \
-                            He reports his sugar levels are better.\n \
-                            He is also following for DM, Atherosclerosis heart disease, and Hyperlipidemia.\n \
-                            **No other medical concerns in today's appointment**.\n 
-                            """
+        Godoy Sergio, a 39-year-old male, presented for a follow-up visit. \n \
+        The patient has a history of HTN, and he is taking his BP readings as an outpatient, his recent BP reading was 120/76, his heart rate was 68, and his current weight is 203 pounds. \n \
+        He reports his sugar levels are better.\n \
+        He is also following for DM, Atherosclerosis heart disease, and Hyperlipidemia.\n \
+        **No other medical concerns in today's appointment**.\n 
+        """
 
         messages_2 = [{'role': 'system', 'content': system_2},
                       {'role': 'user', 'content': f"{self.delimiter}{few_shot_1}{self.delimiter}"},
@@ -98,13 +99,6 @@ class plan_of_care:
     def __init__(self, post_date, delimiter="####"):
         self.post_data = post_date
         self.delimiter = delimiter
-        medication_start = post_date.find("cutformhere:") + len("cutformhere:")
-        medication_end = post_date.find("Doctor dictation")
-        self.medications_text = post_date[medication_start:medication_end].strip()
-        dictation_start = post_date.find("Doctor dictation:") + len("Doctor dictation:")
-        doctor_semi = post_date[dictation_start:].strip()
-        self.diagnosis = extract_text(doctor_semi)
-        self.dictation_final = get_dictation(doctor_semi)
         result = self.final()
         self.result = result
 
@@ -350,7 +344,8 @@ class physical_exam:
     def final(self):
         response_1 = ""
         if "Type of visit: Follow Up" in self.post_data:
-            response_1 = "Limited physical exam. Not in acute distress. Breathing is non-labored, Normal respiratory effort. The effect is normal and appropriate."
+            response_1 = ("Limited physical exam. Not in acute distress. Breathing is non-labored, Normal respiratory "
+                          "effort. The effect is normal and appropriate.")
         else:
             response_1 = "Currently I can only provide physical exam for Type of visit: Fllow Up"
         return response_1
